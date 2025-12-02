6fdbbe1d241357981178169840fae6f6276f86ff
jlbuild <juliabuildbot@gmail.com>
2025-12-02 16:52:17 +0000

    MozillaCACerts_jll build 2025.12.2+0

--- src/MozillaCACerts_jll.jl
+++ src/MozillaCACerts_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("MozillaCACerts")
-JLLWrappers.@generate_main_file("MozillaCACerts", UUID("14a3606d-f60d-562e-9121-12d972cd8159"))
+JLLWrappers.@generate_main_file("MozillaCACerts", Base.UUID("14a3606d-f60d-562e-9121-12d972cd8159"))
 end  # module MozillaCACerts_jll
